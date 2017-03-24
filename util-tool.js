04127fb4397cd588300e577141b56f3e75bb27fe
milemu23 <mlmunksgard@fullsail.edu>
2017-03-23 23:39:11 -0400

    Add version bump / debug

--- util-tool.js
+++ util-tool.js
@@ -1,8 +1,29 @@
 // package to bring in color
 const chalk = require('chalk');
 
-// will be used to save logs to file
-const fs = require('fs');
+const bump = (currentVer, typeofIncrement) => {
+  // major [0]
+  // minor [1]
+  // patch [2]
+  const aryVers = currentVer.split('.');
+
+  for (var i in aryVers) {
+    aryVers[i] = parseInt(aryVers[i], 10);
+  }
+
+  if (typeofIncrement === 'patch') {
+    aryVers[2] += 1;
+  } else if (typeofIncrement === 'minor') {
+    aryVers[2] = 0;
+    aryVers[1] += 1;
+  } else if (typeofIncrement === 'major') {
+    aryVers[2] = 0;
+    aryVers[1] = 0;
+    aryVers[0] += 1;
+  }
+  return aryVers.join('.');
+};
+
 
 // setup output for tool
 const debug = (title, object, status) => {
@@ -42,14 +63,12 @@ const debug = (title, object, status) => {
     case 'err':
     // status is error
       console.error(error('\n' + status + ' ' + msgFormat + '\n'));
-      fs.appendFile('./logs', ('\n' + status + ' ' + msgFormat + '\n'));
       break;
 
     case 'success!':
     case 'success':
     // status is success
       console.log(success('\n' + status + ' ' + msgFormat + '\n'));
-      fs.appendFile('./logs', ('\n' + status + ' ' + msgFormat + '\n'));
       break;
 
     case 'warn':
@@ -57,10 +76,10 @@ const debug = (title, object, status) => {
     case 'warning':
       // status is warning
       console.warn(warn('\n' + status + ' ' + msgFormat + '\n'));
-      fs.appendFile('./logs', ('\n' + status + ' ' + msgFormat + '\n'));
       break;
 
   }
   }
 };
 exports.debug = debug;
+exports.bump = bump;
